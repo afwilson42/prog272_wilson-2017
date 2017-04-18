59e80dc4bd2c72d0fb6d9521392d6897c50249ec
Andrew Wilson at School <andrewf.wilson@bellevuecollege.edu>
2017-04-17 17:57:37 -0700

    restored street, city, state and zip to Address

---
+++ week02-react-UnitTestAddress/src/components/Address.js
@@ -0,0 +1,52 @@
+/* Created by Andrew Wilson on 04/17/2017 ...*/
+
+import React, { Component } from 'react';
+import '../App.css';
+
+class Address extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: this.props.firstName,
+            lastName: "",
+            streetAdr: "",
+            city: "",
+            state:"",
+            zip:"",
+            phone: ""
+        }
+    }
+
+    setAddress = () => {
+        this.setState({
+            firstName: 'Carly',
+            lastName: 'bar',
+            streetAdr: 'PO Box 40600',
+            city: 'Olympia',
+            usState:'WA',
+            zip: '98504-0600',
+            phone: '(360) 786-7848'
+
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+
+                <p className="App-intro">
+                    First Name: {this.state.firstName}
+                </p>
+
+                <p className="App-intro">
+                    Last Name: {this.state.lastName}
+                </p>
+
+
+                <button onClick={this.setAddress} className="setAdr">Set Address</button>
+            </div>
+        );
+    }
+}
+
+export default Address;
