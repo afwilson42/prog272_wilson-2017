7bb6116ef0bc9389c258ffb5e0f2a8a6d9d0dd8a
Andrew Wilson at School <andrewf.wilson@bellevuecollege.edu>
2017-05-01 18:28:21 -0700

    redoing CSS change additions, didnt commit before

--- CongressAddress/src/index.js
+++ CongressAddress/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SmallNumbers from './components/SmallNumbers';
 import './css/index.css';
 import Address from './components/Address';
 import ElfHeader from './components/ElfHeader';
-
+import AddressMenu from './components/AddressMenu';
 
 ReactDOM.render(
     <div>
         <ElfHeader />
-        <App />
-        <Address />
+        <AddressMenu />
     </div>,
     document.getElementById('root')
 );
