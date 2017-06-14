b040e2815039dfb5229a6844ebce4ee29d07426d
Andrew Wilson at School <andrewf.wilson@bellevuecollege.edu>
2017-06-14 15:53:06 -0700

    missed Expo update and AddressMenu update

--- CongressAddress/src/components/AddressMenu.js
+++ CongressAddress/src/components/AddressMenu.js
@@ -7,19 +7,53 @@ import {
         Link
 } from 'react-router-dom';
 import '../css/App.css';
+import { MenuItem, Nav, Navbar, NavDropdown, NavItem   } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class AddressMenu extends Component {
 
     render() {
+
+        const navbarInstance = (
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <LinkContainer to='/'><NavItem>CongressAddress</NavItem></LinkContainer>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to='/'><NavItem>Address Show</NavItem></LinkContainer>
+                        <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href='#'>Link Right</NavItem>
+                        <NavItem eventKey={2} href='#'>Link Right</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+
         return (
 
-            <div className='AppLink'>
+                <div>
+                    {navbarInstance}
+                </div>
+
+            /*<div className='AppLink'>
                 <ul>
                     <li><Link to='/'>AddressShow</Link></li>
                     <li><Link to='/edit'>AddressEdit</Link></li>
                     <li><Link to='/small'>SmallNumbers</Link></li>
                 </ul>
-            </div>
+            </div> */
 
         );
     }
--- Expo/expo_wilson/App.js
+++ Expo/expo_wilson/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, Button, View } from 'react-native';
+import Address from './Address';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Wilson on Prog 272 Expo</Text>
-          <Button
-              onPress={this.fetchAddress}
-              title="Get Data"
-              color="#841584"
-              accessibilityLabel="Learn more about this purple button"
-          />
+
+        <Address/>
       </View>
 
     );
